11469b663b738f9ce65ab659cc6459e32a7e8e54
ThoNT <truongthonguyen107@gmail.com>
2026-06-10 15:04:02 +0700

    feat: login UI [thont]

---
+++ src/features/auth/components/AuthForm.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { PasswordInput } from "@/components/ui/password-input";
+
+export default function AuthForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+
+    try {
+        console.log("Login")
+
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
+  return (
+    <Card className="w-full max-w-md">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Enter your email and password to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <form
+          className="space-y-4"
+          onSubmit={handleLogin}
+        >
+          <div className="space-y-2">
+            <Label htmlFor="email">
+              Email
+            </Label>
+
+            <Input
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) =>
+                setEmail(e.target.value)
+              }
+            />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="password">
+              Password
+            </Label>
+
+            <PasswordInput
+              id="password"
+              placeholder="••••••••"
+              value={password}
+              onChange={(e) =>
+                setPassword(e.target.value)
+              }
+            />
+          </div>
+
+          <Button
+            type="submit"
+            className="w-full"
+          >
+            Sign In
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
