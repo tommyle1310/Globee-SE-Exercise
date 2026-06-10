bfc5bb55ac29a37f81ea2be87f32ca74eb8195bf
tommyle1310 <167192084+tommyle1310@users.noreply.github.com>
2026-06-10 15:20:49 +0700

    feat: dashboard (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import AppRouter from "./routes";
 import AppLayout from "./components/app-layout/AppLayout";
 
 export function App() {
-  return <>
-  <AppLayout>
-    <AppRouter />
-  </AppLayout>
-  </>;
+  return     <AppRouter />
 
 }
 
