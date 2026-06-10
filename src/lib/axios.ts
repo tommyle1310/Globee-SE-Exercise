088509246a332fc8f8ec2c444f615c31827e2100
Le Quy Hoang Tam <natural01203@gmail.com>
2026-06-10 16:28:08 +0700

    enhance protected page

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "@/stores/useAuthStore";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -8,5 +9,15 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().logout();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
